93d241d5a695860002259adb1c9de482ffdc7591
yxxhero <aiopsclub@163.com>
2023-09-04 10:09:47 +0800

    rename ReFormatNeeds to reformatNeeds
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -435,7 +435,7 @@ var DefaultFetchOutputDirTemplate = filepath.Join(
 	"{{ or .Release.Version \"latest\" }}",
 )
 
-func (st *HelmState) ReFormatNeeds(spec *ReleaseSpec) []string {
+func (st *HelmState) reformat(spec *ReleaseSpec) []string {
 	var needs []string
 	releaseInstalledInfo := make(map[string]bool)
 	for _, r := range st.OrginReleases {
@@ -500,7 +500,7 @@ func (st *HelmState) ApplyOverrides(spec *ReleaseSpec) {
 		spec.Namespace = st.OverrideNamespace
 	}
 
-	spec.Needs = st.ReFormatNeeds(spec)
+	spec.Needs = st.reformat(spec)
 }
 
 type RepoUpdater interface {
